ed077d633309b8c1971371607100d07d7b1156d5
flam1ngo <kris.s.03@mail.ru>
2024-06-04 14:49:42 +0300

    Initial commit

---
+++ STL_Task2/STL_Task2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <set>
+
+int main() {
+    int n;
+
+    std::cout << "[IN]:" << std::endl;
+    std::cin >> n;
+
+    std::set<int> s;
+    for (int i = 0; i < n; i++) {
+        int x;
+        std::cin >> x;
+        s.insert(x);
+    }
+
+    std::cout << "[OUT]:" << std::endl;
+    for (auto it = s.rbegin(); it != s.rend(); it++) {
+        std::cout << *it << std::endl;
+    }
+
+    return 0;
+}
